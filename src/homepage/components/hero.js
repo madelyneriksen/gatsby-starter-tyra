87a0cf9a285432d7a9988143732103f38a82dbb4
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-16 10:36:45 -0800

    Updated homepage hero unit.

--- src/homepage/components/hero.js
+++ src/homepage/components/hero.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Img from 'gatsby-image';
+import { Link } from 'gatsby';
 import '../../common/styles/custom.tachyons.css'
 import 'tachyons';
 
@@ -10,9 +11,10 @@ export default (props) => (
       fluid={props.image}
       alt={props.description} />
     <div
-      className="h-auto bg-near-white mw7 w-100 flex flex-column items-center justify-center pv4 ph2 center tl-75-l">
-      <h1 className="fw1 display dark-gray db tc w-100 mw6 f3 f2-ns">{props.title}</h1>
-      <p className="serif mw6 tc f5 dn dib-l">{props.description}</p>
+      className="h-auto bg-near-white mw9 w-100 flex flex-column items-center justify-center pv4 ph2 center">
+      <h1 className="fw1 display dark-gray db tc w-100 mw7 f3 f2-ns">{props.title}</h1>
+      <p className="serif mw6 tc f5 dn dib-l mb4 db">{props.description}</p>
+      <Link className="db pv3 ph5 tracked ttu b bg-washed-red dark-gray sans-serif no-underline" to={props.to}>Read It</Link>
     </div>
   </React.Fragment>
 )
