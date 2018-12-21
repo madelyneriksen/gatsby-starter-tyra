8e473ae78ac9d4b75095b3229fd7e467dd1ce5ce
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-21 06:03:36 -0800

    Updated typography

--- src/blog/components/hero.js
+++ src/blog/components/hero.js
@@ -5,9 +5,9 @@ import "tachyons";
 
 export default props => (
   <div className="bg-washed-red ph2 pv5 flex flex-column justify-center items-center">
-    <Link href={"/" + props.category} className="sans-serif ttu mid-gray tracked f5">{props.category}</Link>
+    <Link to={`/${props.category}`} className="sans-serif ttu mid-gray tracked f6">{props.category}</Link>
     <h1 className="dark-gray display fw4 f1-l f2 tc">{props.title}</h1>
-    <span className="sans-serif tracked ttu f5 mb2">by {props.author}</span>
-    <span className="sans-serif tracked ttu f5">{props.date}</span>
+    <span className="sans-serif tracked ttu f6 mb2">by {props.author}</span>
+    <span className="sans-serif tracked ttu f6">{props.date}</span>
   </div>
 )
